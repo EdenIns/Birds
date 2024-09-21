fc8978bd06c58357bdcff45441b71b5e253820af
EdenIns <e.oudaya@outlook.com>
2024-09-21 19:07:35 +0200

    add footer + style App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,36 @@
 <template>
-  <Header />
-  <BContainer>
-    <RouterView />
-  </BContainer>
-  <Footer />
+  <div class="wrapper">
+    <Header />
+    <BContainer class="content">
+      <RouterView />
+    </BContainer>
+    <Footer />
+  </div>
 </template>
 
 <script setup>
 import { BContainer } from 'bootstrap-vue-next';
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 import { RouterView } from 'vue-router';
-
 </script>
+
+<style scoped>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+</style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="footer">
+        <p class="me-3">©2024-Birds</p>
+    </div>
+
+</template>
+<script setup>
+
+</script>
+<style>
+
+.footer {
+    background-color: #7105bc;
+    color: white;
+    height: 50px;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+}
+
+</style>
